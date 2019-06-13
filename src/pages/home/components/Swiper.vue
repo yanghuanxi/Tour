0f82035d08c6e43f632ef94b90133286cccbd9cb
yanghuanxi <bilei_lei@163.com>
2019-06-13 10:47:04 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wraaper">
+	  <swiper :options="swiperOption">
+	    <!-- slides -->
+	    <swiper-slide v-for="item of swiperList" :key="item.id">
+	    	<img class="swiper-img" :src="item.imgUrl" />
+	    </swiper-slide>
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeSwiper',
+		data: function() {
+			return {
+				swiperOption: {
+					pagination: '.swiper-pagination',
+					loop: true
+				},
+				swiperList: [{
+					id: '0001',
+					imgUrl: "https://source.qunarzz.com/site/images/wns/2019505_dujia_homepage_top_banner_5.jpg"
+				}, {
+					id: '0002',
+					imgUrl: "https://source.qunarzz.com/site/images/wns/20190530_qunar_dujia_homepage_top_banner_1.jpg"
+				}, {
+					id: '0003',
+					imgUrl: "https://source.qunarzz.com/site/images/wns/20190531_dujia_homepage_top_banner_4.jpg"
+				}]
+			}
+		}
+	}
+</script>
+<style lang="stylus" scoped>
+	.wraaper >>> .swiper-pagination-bullet-active
+	  background: #fff
+	.wraaper
+	  overflow: hidden
+	  width: 100%
+	  height: 0
+	  padding-bottom: 25.6%
+	  background: #eee
+	  .swiper-img
+	     width: 100%
+</style>
