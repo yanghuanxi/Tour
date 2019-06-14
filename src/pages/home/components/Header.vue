e995aaa35e5212b59e2d15fc4c51ccab5c04a9b4
yanghuanxi <bilei_lei@163.com>
2019-06-14 11:24:30 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,10 +6,12 @@
 	  <div class="header-input">
 		<span class="iconfont">&#xe632;</span>
 	    输入城市/景点/游玩主题</div>
+	  <router-link to="/city">
 	  <div class="header-right">
 	  	{{this.city}}
 	  	<span class="iconfont  arrow-icon">&#xe64a;</span>
 	  </div>
+	  </router-link>
 	</div>
 </template>
 
@@ -26,8 +28,8 @@
 	@import '~styles/varibles.styl'
 	.header
 		display: flex
-		height: .86rem
-		line-height: .86rem
+		height: $headerHeight
+		line-height: $headerHeight
 		background: $bgColor
 		color: #fff
 		.header-left
@@ -50,6 +52,7 @@
 			width: 1.24rem
 			float: right
 			text-align: center
+			color: #fff
 			.arrow-icon
 				margin-left: -.04rem
 				font-size: .24rem
