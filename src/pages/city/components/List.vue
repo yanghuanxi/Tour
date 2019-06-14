4f0e3430212831cd0fa2162da3bffa67bf78994e
yanghuanxi <bilei_lei@163.com>
2019-06-14 22:11:04 +0800

    use vuex

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -4,14 +4,15 @@
 		<div class="area"><div class="title border-topbottom">当前城市</div></div>
 		<div class="button-list">
 			<div class="button-wrapper">
-				<div class="button">北京</div>
+				<div class="button">{{this.currentCity}}</div>
 			</div>
 		</div>
 		<div class="area"><div class="title border-topbottom">热门城市</div></div>
 			<div class="button-list">
 				<div class="button-wrapper"
 					 v-for="item of hot"
-					 :key="item.id">
+					 :key="item.id"
+					 @click="handleCityClick(item.name)">
 					<div class="button">{{item.name}}</div>
 				</div>
 			</div>
@@ -24,7 +25,8 @@
 				<div 
 				class="item border-bottom"
 				v-for="innerItem of  item"
-				:key="innerItem.id">
+				:key="innerItem.id"
+				@click="handleCityClick(innerItem.name)">
 
 				{{innerItem.name}}</div>
 			</div>
@@ -35,6 +37,7 @@
 
 <script>
 	import Bscroll from 'better-scroll'
+	import { mapState, mapMutations} from 'vuex'
 	export default {
 		name: 'CityList',
 		props: {
@@ -42,10 +45,17 @@
 			cities: Object,
 			letter: String
 		},
-		mounted () {
-			this.scroll = new Bscroll(this.$refs.wrapper, {
-				click: true
+		computed: {
+			...mapState({
+				currentCity: 'city'
 			})
+		},
+		methods: {
+			handleCityClick (city) {
+				this.changeCity(city)
+				this.$router.push('/')
+			},
+			...mapMutations(['changeCity'])
 		},
 		watch: {
 			letter () {
@@ -54,6 +64,11 @@
 					this.scroll.scrollToElement(element)
 				}	
 			}
+		},
+		mounted () {
+			this.scroll = new Bscroll(this.$refs.wrapper, {
+				click: true
+			})
 		}
 	}
 </script>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -5,7 +5,10 @@
 		</div>
 		<div class="search-content" ref="search" v-show="keyword">
 			<ul>
-				<li  class="search-item border-bottom" v-for="item of list" :key="item.id">
+				<li  class="search-item border-bottom" 
+				     v-for="item of list" 
+				     :key="item.id" 
+				     @click="handleCityClick(item.name)">
 					{{item.name}}
 				</li>
 				<li class="search-item border-bottom" v-show="hasNoData">
@@ -18,6 +21,7 @@
 
 <script>
 	import Bscroll from 'better-scroll'
+	import { mapMutations } from 'vuex'
 	export default {
 		name: 'CitySearch',
 		props: {
@@ -56,6 +60,13 @@
 					this.list = result
 				}, 100)
 			}
+		},
+		methods: {
+			handleCityClick (city) {
+				this.changeCity(city)
+				this.$router.push('/')
+			},
+			...mapMutations(['changeCity'])
 		},
 		mounted () {
 			this.scroll = new Bscroll(this.$refs.search)
