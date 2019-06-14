953526768448904658c9b1bd790cb0026b552450
yanghuanxi <bilei_lei@163.com>
2019-06-14 18:08:38 +0800

    perfomance

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -15,7 +15,10 @@
 					<div class="button">{{item.name}}</div>
 				</div>
 			</div>
-		<div class="area" v-for="(item, key) of cities" :key="key">
+		<div class="area" 
+		     v-for="(item, key) of cities" 
+		     :key="key"
+		     :ref="key">
 			<div class="title border-topbottom">{{key}}</div>
 			<div class="item-list">
 				<div 
@@ -36,10 +39,21 @@
 		name: 'CityList',
 		props: {
 			hot: Array,
-			cities: Object
+			cities: Object,
+			letter: String
 		},
 		mounted () {
-			this.scroll = new Bscroll(this.$refs.wrapper)
+			this.scroll = new Bscroll(this.$refs.wrapper, {
+				click: true
+			})
+		},
+		watch: {
+			letter () {
+				if (this.letter) {
+					const element = this.$refs[this.letter][0]
+					this.scroll.scrollToElement(element)
+				}	
+			}
 		}
 	}
 </script>
