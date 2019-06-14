d62511fa250646214080e4d7e38d2be2fa6c3e8e
yanghuanxi <bilei_lei@163.com>
2019-06-14 23:17:06 +0800

    finish

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,6 @@ Vue.use(Vuex)
 
 export default new  Vuex.Store({
 	state: state,
-	mutations: mutations,
-	getters: {
-		doubleCity (state) {
-			return state.city + ' ' + state.city
-		}
-	}
+	mutations: mutations
 })
 
