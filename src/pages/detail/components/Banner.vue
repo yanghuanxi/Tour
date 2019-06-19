11cf8e1e109966d2c8bbf08c1ae01af7d3f31c0b
yanghuanxi <bilei_lei@163.com>
2019-06-19 19:58:40 +0800

    datail ajaxdetailanimation

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -7,12 +7,15 @@
 				<div class="banner-number"><span class="iconfont banner-icon">&#xe632;</span>{{this.bannerImgs.length}}</div>
 			</div>
 		</div>
+		<fade-animation>
 		<common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+		</fade-animation>
 	</div>
 </template>
 
 <script>
 	import CommonGallary from 'common/gallary/Gallary'
+	import FadeAnimation from 'common/fade/FadeAnimation'
 	export default {
 		name: 'DetailBanner',
 		props: {
@@ -35,7 +38,8 @@
 			}
 		},
 		components: {
-			CommonGallary
+			CommonGallary,
+			FadeAnimation
 		}
 	}
 </script>
